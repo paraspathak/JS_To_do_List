8728d91f01c6c6c2f90e87b9f8dd9f0540951b54
Paras Pathak <paras.pathak@mavs.uta.edu>
2019-05-26 14:04:55 -0500

    Delete and delete all working

--- controller.js
+++ controller.js
@@ -2,7 +2,19 @@
 window.addEventListener('keydown', listen_keystrokes, false)
 
 var number_of_item = 0;
-var edit_mode = false;
+var delete_mode = false;
+
+//removing rows when delete is pressed
+Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+}
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+    for(var i = this.length - 1; i >= 0; i--) {
+        if(this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+}
 
 //search for copying the URL as a todo list
 function listen_keystrokes(e){
@@ -12,36 +24,26 @@ function listen_keystrokes(e){
     }
 }
 
-function remove_todo(id){
-    //var element = document.getElementById(id);
-    //element.style.color = "green" ;
-    //element.style.setProperty("text-decoration","line-through");
-    alert("ASdsa");
-}
-
-function edit_todo(){
-    for (var i =0; i< number_of_item; i++){
-
-    }
-}
-
+//working function that creates the todo item
 function make_row_cleaner(data_to_put_in_row){
-    var new_row = document.createElement('tr');
+    var new_row = document.createElement('tr');     //create a new row
     new_row.className='row';
     new_row.id = 'row'+number_of_item.toString();
         
-        var first_column = document.createElement('td');
+        var first_column = document.createElement('td');    //first column
         first_column.className = 'column';
-            var new_button = document.createElement('button');
-            new_button.id = number_of_item.toString();
-            new_button.className = 'notdone';
+            var new_button = document.createElement('button');  //button
+            new_button.id = number_of_item.toString();  //stores the number of the button (0...n-1)
+            new_button.className = 'notdone'; //to either display checkmark or circle
             new_button.innerHTML = "o";
             new_button.onclick = function() {
-                if(edit_mode){
-                    remove_todo(new_button.id);
+                if(delete_mode){
+                    remove_todo(new_button.id); //when user wants to delete the node
+                    delete_mode = false;
+                    document.getElementById("delete_button").checked = false;
                 }
-                else{
-                    if(new_button.className=='notdone'){
+                else{   //swaps between two images and changes color of the corresponding todo item
+                    if(new_button.className=='notdone'){    
                         new_button.innerHTML = "&#10003";   //&#10006 is for cross mark
                         new_button.className = 'done';
                         document.getElementById(('div'+new_button.id.toString())).style.color="green";
@@ -49,6 +51,7 @@ function make_row_cleaner(data_to_put_in_row){
                     else if(new_button.className=='done'){
                         new_button.innerHTML = "o";
                         new_button.className = 'notdone';
+                        document.getElementById(('div'+new_button.id.toString())).style.color="purple";
                     }
                 }   
             }
@@ -70,7 +73,7 @@ function make_row_cleaner(data_to_put_in_row){
     document.getElementById("add_data").value='';
 }
 
-
+//function that creates a new todo items, plus button or enter is pressed to call on here
 function create_new_todo_item() {
     if(document.getElementById("add_data").value===""){
         document.getElementById("add_data").placeholder="Please enter new todo!!";
@@ -82,13 +85,14 @@ function create_new_todo_item() {
     }
     
 }
-
+//function that gets called to create a new todo item with the websites current link
 function create_new_todo_item_link(item_name){
     make_row_cleaner(item_name);
     document.getElementById(('div'+number_of_item.toString())).style.color="blue";
     number_of_item +=1;
 }
 
+//handler for the case when enter is pressed during adding todo items
 function search(ele){
     if(event.key==='Enter'){
         create_new_todo_item();
@@ -96,3 +100,22 @@ function search(ele){
     }
 }
 
+function remove_todo(id){
+    document.getElementById((id)).remove();
+}
+
+//Deletes all the todo items
+function delete_all_nodes(){
+    for (var i =0; i< number_of_item; i++){
+        remove_todo(('row'+i.toString()));
+    }
+    delete_mode = false;
+    document.getElementById("delete_button").checked = false;
+}
+
+
+function edit_todo(){
+    for (var i =0; i< number_of_item; i++){
+
+    }
+}
