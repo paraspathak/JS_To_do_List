a704057f48154c3a6f4f19ff2a92720c92c171c4
Paras Pathak <paraspathak@outlook.com>
2019-05-31 15:53:29 -0500

    Switched to a OOP model

--- popup.js
+++ popup.js
@@ -1,70 +1,17 @@
-
-var number_of_item = 0;
+//Global boolean to know if delete mode or not
 var delete_mode = false;
-messages = [];
-colors =  [];
-buttons = [];
 
-//store the number of items
-function increase_number_of_items(){
-    chrome.storage.sync.set({"number":number_of_item},function(items){
-        console.log("Number of items is stored");
-    });    
-}
+//Event Listeners
 
-//store the todo item, color and type of button
-function store_todo(message_, color_, button_){
-    messages.push(message_);
-    colors.push(color_);
-    buttons.push(button_);
-    chrome.storage.sync.set({"messages_stored":messages,
-                             "colors_stored":colors,
-                             "buttons_stored":buttons},function(){
-        console.log("Number of items is stored");
-    });
-}
-//get the number of items stored
-chrome.storage.sync.get("number",function(items){
-    if (!chrome.runtime.error) {
-        console.log(items);
-        if(items.number){
-            console.log(items.number);
-            //number_of_item = items.number;
-        }
-      }
-});
-
-
-chrome.storage.sync.get(["messages_stored","colors_stored","buttons_stored"],function(items){
-    if (!chrome.runtime.error) {
-        if(items.messages_stored && items.colors_stored && items.buttons_stored){
-            var i = 0;
-            console.log(items.messages_stored[i], items.colors_stored[i], items.buttons_stored[i]);
-        }
-        console.log(items);
-      }
-});
-chrome.storage.sync.get("colors_stored",function(items){
-    if (!chrome.runtime.error) {
-        console.log(items);
-      }
-});
-chrome.storage.sync.get("buttons_stored",function(items){
-    if (!chrome.runtime.error) {
-        console.log(items);
-      }
-});
-
-//when the plus icon is clicked to start a new todo item
+//plus icon
 document.getElementById("create_new_todo_item").addEventListener("click",
     function(){
         console.log("newtodo item call block");
-        create_new_todo_item();
-        
+        create_new_todo_item();        
     }
 );
 
-//entered is pressed to store new data
+//enter
 document.getElementById("add_data").addEventListener("keypress",
     function(event){
         if(event.key=='Enter'){
@@ -75,17 +22,17 @@ document.getElementById("add_data").addEventListener("keypress",
     }
 );
 
-//delete button is pressed
+//delete button
 document.getElementById("delete_button").addEventListener("click", 
 function(){
     console.log("delete button signal clicked");
     delete_mode = !delete_mode;
 });
 
-//delete all button is pressed
+//delete all button
 document.getElementById("delete_all_button").addEventListener("click", 
 function(){
-    for (var i =0; i< number_of_item; i++){
+    for (var i =0; i< current_application.next_number; i++){
         remove_todo(('row'+i.toString()));
     }
     chrome.storage.sync.clear(function(){
@@ -93,48 +40,61 @@ function(){
     });
     delete_mode = false;
     console.log("Delete all ");
-    document.getElementById("delete_button").checked = false;
 });
 
-
-function remove_todo(id){
-    el = document.getElementById((id));
-    if(el){
-        el.remove();
+//Classes 
+class Todo_item {
+    constructor(number, message, color, button) {
+        this.id_number = number;
+        this.todo_message = message;
+        this.todo_color = color;
+        this.todo_button = button;
     }
-}
-
-
-function make_row_cleaner(data_to_put_in_row){
-    console.log("Inside make row cleaner");
-    store_todo(data_to_put_in_row,"red","o");   //store the new data as red and O button
-    var new_row = document.createElement('tr');     //create a new row
-    new_row.className='row';
-    new_row.id = 'row'+number_of_item.toString();
+    add_to_html(){
+        //Create a new_row in the html file
+        var new_row = document.createElement('tr');     //create a new row
+        new_row.className='row';
+        new_row.id = 'row'+this.id_number.toString();  //row1, row2, ...
         
+        //Create first Column
         var first_column = document.createElement('td');    //first column
         first_column.className = 'column';
             var new_button = document.createElement('button');  //button
-            new_button.id = number_of_item.toString();  //stores the number of the button (0...n-1)
-            new_button.className = 'notdone'; //to either display checkmark or circle
-            new_button.innerHTML = "o";
+            new_button.id = this.id_number.toString();  //stores the number of the button (0...n-1)
+            if(this.todo_button==="o"){
+                new_button.className = 'notdone'; //to either display checkmark or circle
+                new_button.innerHTML = "o";
+            }
+            else {
+                new_button.className = 'done'; //to either display checkmark or circle
+                new_button.innerHTML = "&#10003";
+            }            
             new_button.onclick = function() {
                 if(delete_mode){
                     remove_todo(('row'+new_button.id.toString())); //when user wants to delete the node
                     delete_mode = false;
-                    document.getElementById("delete_button").checked = false;
+                    document.getElementById("delete_button").checked = false;   //uncheck the delete mode
                 }
                 else{   //swaps between two images and changes color of the corresponding todo item
                     if(new_button.className=='notdone'){    
-                        new_button.innerHTML = "&#10003";   //&#10006 is for cross mark
+                        new_button.innerHTML = "&#10003";   
+                        this.todo_button = "&#10003";   
+
                         //change the stored button as well
                         new_button.className = 'done';
+                        
+                        //Change the color of the element as well
                         document.getElementById(('div'+new_button.id.toString())).style.color="green";
+                        this.todo_color = "green";
                     }
                     else if(new_button.className=='done'){
                         new_button.innerHTML = "o";
+                        this.todo_button = "o";
+
                         new_button.className = 'notdone';
+                        
                         document.getElementById(('div'+new_button.id.toString())).style.color="purple";
+                        this.todo_color = "purple";
                     }
                 }   
             }
@@ -143,10 +103,22 @@ function make_row_cleaner(data_to_put_in_row){
         var second_column = document.createElement('td');
         second_column.className = 'column';
             var new_division = document.createElement('div');
-            new_division.className = number_of_item.toString();
-            new_division.id = 'div'+number_of_item.toString();
-            new_division.innerHTML = data_to_put_in_row;
-            new_division.contentEditable = true;
+            new_division.className = this.id_number.toString();
+            new_division.id = 'div'+ this.id_number.toString();
+            
+            //Set the color and message
+            new_division.style.color = this.todo_color;
+            
+            /*
+            if(this.message==undefined){
+                console.log("else block", this.message);
+                this.message = document.getElementById("add_data").value;
+                console.log("after", this.message);
+            }*/
+
+            new_division.innerHTML = this.todo_message;
+            
+            new_division.contentEditable = true;        //TODO!-need to track if the content is changed or not
             new_division.onmouseover = new_division.style.cursor = 'pointer';
         second_column.appendChild(new_division);
 
@@ -155,19 +127,44 @@ function make_row_cleaner(data_to_put_in_row){
     document.getElementById("content").appendChild(new_row);
     document.getElementById("add_data").placeholder="Enter a ToDo item";
     document.getElementById("add_data").value='';
+    }
 }
 
+class Todo_Application {
+    constructor(starting_value){
+        this.next_number = starting_value;
+        this.all_todo_items = [];
+    }
+    add_todo_item(item){
+        this.all_todo_items.push(item);
+        this.next_number +=1;
+    }
+}
+
+// Helper functions
+function remove_todo(id){
+    el = document.getElementById((id));
+    if(el){
+        el.remove();
+    }
+}
+
+//Creates todo item by getting value of text entry box
 function create_new_todo_item(){
-    console.log("Inside create new todo");
     if(document.getElementById("add_data").value===""){
         document.getElementById("add_data").placeholder="Please enter new todo!!";
     }
     else {
-        make_row_cleaner(document.getElementById("add_data").value);
-        document.getElementById(('div'+number_of_item.toString())).style.color="red";
-        number_of_item += 1;
-        increase_number_of_items();
+        //Create a new instance of a todo item
+        var data_from_text_box = document.getElementById("add_data").value;
+        console.log(data_from_text_box);
+        var temp = new Todo_item(current_application.next_number,data_from_text_box,"red","o");
+        temp.add_to_html(); //Add the item to the html
+        
+        //Add the item to the current class
+        current_application.add_todo_item(temp);   
     }
 }
 
-
+//Instance a new application
+var current_application = new Todo_Application(0);
